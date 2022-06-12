1ef436814ef2e065f89b507c06912053206779ae
kcrane <kcrane121@gmail.com>
2022-06-12 15:51:51 -0400

    configure entrypoint to create_db on startup of flask service via docker-compose

---
+++ services/web/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Verify Postgres is up and healthy
+# Note: File permissions requirement
+# - chmod +x services/web/entrypoint.sh
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py create_db
+
+exec "$@"

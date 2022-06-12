a9288e66c920cbaa6f39d5caab95f1a4dded6e17
kcrane <kcrane121@gmail.com>
2022-06-12 15:17:11 -0400

    initial setup of db with docker

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,9 +1,17 @@
 from flask.cli import FlaskGroup
-
-from project import app
+from project import app, db
 
 
 cli = FlaskGroup(app)
+
+
+# Enables the use of cli commands to be run alongside docker-compose
+# eg: docker-compose exec web python manage.py create_db
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 
 if __name__ == "__main__":
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,7 +1,24 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
+# Configure app to load config from config.py Config
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
+
+
+# Database model
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, email):
+        self.email = email
 
 
 @app.route("/")
